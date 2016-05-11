dab9df3c11b55a4915608d0fc1015dad36f8d532
Christer van der Meeren <cmeeren@gmail.com>
2016-05-11 22:14:55 +0200

    revert property to private

--- gr-progress.php
+++ gr-progress.php
@@ -21,7 +21,7 @@ class gr_progress_cvdm_widget extends WP_Widget {
     private $CURRENTLY_READING_SHELF_KEY = 'currentlyReadingShelf';
     private $ADDITIONAL_SHELF_KEY = 'additionalShelf';
     private $SECONDS_TO_WAIT_AFTER_FAILED_FETCH = 3600;
-    public $DEFAULT_SETTINGS = [
+    private $DEFAULT_SETTINGS = [
         'title' => 'Currently reading',
         'goodreadsAttribution' => 'Data from Goodreads',
         'userid' => '',
