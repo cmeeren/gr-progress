cb382f5065f74fcb259fb2bd7ba367b05c5286c3
Christer van der Meeren <cmeeren@gmail.com>
2016-05-26 21:53:01 +0200

    Check PHP version when activating plugin

---
+++ src/gr-progress/gr-progress-widget.php
@@ -0,0 +1,48 @@
+<?php
+
+/* Plugin Name: GR Progress
+  Description: Displays shelves and reading progress from Goodreads.
+  Version: 1.0.0
+  Author: Christer van der Meeren
+  Author URI: http://relativisticramblings.com
+  License: GPLv2 or later
+ */
+
+namespace relativisticramblings\gr_progress;
+
+require_once('includes/gr_progress_cvdm_backend.php');
+
+class gr_progress_cvdm_widget extends \WP_Widget {
+
+    public $widget;
+
+    function __construct() {
+        parent::__construct(
+                'gr_progress_cvdm_widget', 'GR Progress', ['description' => 'Displays reading progress and shelves from Goodreads.']
+        );
+        $this->widget = new gr_progress_cvdm_backend($this);
+    }
+
+    public function widget($args, $instance) {
+        $this->widget->printWidget($args, $instance);
+    }
+
+    public function form($instance) {
+        $this->widget->printForm($instance);
+    }
+
+    public function update($new_instance, $old_instance) {
+        return $this->widget->getNewWidgetSettings($new_instance, $old_instance);
+    }
+
+}
+
+/**
+ * @codeCoverageIgnore
+ */
+function load_widget() {
+    register_widget('relativisticramblings\gr_progress\gr_progress_cvdm_widget');
+    wp_enqueue_style('gr-progress-cvdm-style-default', plugin_dir_url(__FILE__) . 'css/style.css');
+}
+
+add_action('widgets_init', 'relativisticramblings\gr_progress\load_widget');
--- src/gr-progress/gr-progress.php
+++ src/gr-progress/gr-progress.php
@@ -8,41 +8,11 @@
   License: GPLv2 or later
  */
 
-namespace relativisticramblings\gr_progress;
-
-require_once('includes/gr_progress_cvdm_backend.php');
-
-class gr_progress_cvdm_widget extends \WP_Widget {
-
-    public $widget;
-
-    function __construct() {
-        parent::__construct(
-                'gr_progress_cvdm_widget', 'GR Progress', ['description' => 'Displays reading progress and shelves from Goodreads.']
-        );
-        $this->widget = new gr_progress_cvdm_backend($this);
-    }
-
-    public function widget($args, $instance) {
-        $this->widget->printWidget($args, $instance);
-    }
-
-    public function form($instance) {
-        $this->widget->printForm($instance);
-    }
-
-    public function update($new_instance, $old_instance) {
-        return $this->widget->getNewWidgetSettings($new_instance, $old_instance);
-    }
-
+if (version_compare(PHP_VERSION, '5.4', '<')) {
+    add_action('admin_notices',
+            create_function('',
+                    "echo '<div class=\"error\"><p>GR Progress requires PHP 5.4 to function properly. Please upgrade PHP or deactivate GR Progress.</p></div>';"));
+    return;
+} else {
+    require 'gr-progress-widget.php';
 }
-
-/**
- * @codeCoverageIgnore
- */
-function load_widget() {
-    register_widget('relativisticramblings\gr_progress\gr_progress_cvdm_widget');
-    wp_enqueue_style('gr-progress-cvdm-style-default', plugin_dir_url(__FILE__) . 'css/style.css');
-}
-
-add_action('widgets_init', 'relativisticramblings\gr_progress\load_widget');
